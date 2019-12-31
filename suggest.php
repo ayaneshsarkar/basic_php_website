b33cb714740b2fe00878eacd666dba1faad75a6d
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-01-01 00:56:18 +0530

    Initial commit

--- suggest.php
+++ suggest.php
@@ -1,5 +1,15 @@
 <?php 
 
+// Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   $name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
@@ -16,13 +26,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit;
   }
 
-  echo "<pre>";
+  if (!PHPMailer::validateAddress($email)) {
+    echo "Invalid Email Address";
+    exit;
+  }
+
   $email_body = "";
   $email_body .= "Name: $name\n";
   $email_body .= "Email: $email\n";
   $email_body .= "Details: $details\n";
-  echo $email_body;
-  echo "</pre>";
+
+  // Instantiation and passing `true` enables exceptions
+  $mail = new PHPMailer;
+
+  //Tell PHPMailer to use SMTP
+  $mail->isSMTP();
+  //Enable SMTP debugging
+  // SMTP::DEBUG_OFF = off (for production use)
+  // SMTP::DEBUG_CLIENT = client messages
+  // SMTP::DEBUG_SERVER = client and server messages
+  $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+  //Set the hostname of the mail server
+  $mail->Host = 'smtp.gmail.com';
+  // use
+  // $mail->Host = gethostbyname('smtp.gmail.com');
+  // if your network does not support SMTP over IPv6
+  //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+  $mail->Port = 587;
+  //Set the encryption mechanism to use - STARTTLS or SMTPS
+  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+  //Whether to use SMTP authentication
+  $mail->SMTPAuth = true;
+  //Username to use for SMTP authentication - use full email address for gmail
+  $mail->Username = 'ayaneshsarkar101@gmail.com';
+  //Password to use for SMTP authentication
+  $mail->Password = 'ctqwwyprgcaxntlo';
+
+  $mail->setFrom('ayaneshsarkar101@gmail.com', $name);
+  $mail->addReplyTo($email, $name);
+  $mail->addAddress('ayaneshsarkar101@gmail.com', 'Ayanesh Sarkar');
+  $mail->Subject  = 'Library Suggesstion from ' . $name;
+  $mail->Body     = $email_body;
+  if(!$mail->send()) {
+    echo 'Mailer error: ' . $mail->ErrorInfo;
+    exit;
+  }
 
   // To do: Send email 
   header("location:suggest.php?status=thanks"); 
