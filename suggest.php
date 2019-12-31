94441550cdf1e3a5a0756e0c66d4ed2f4a4acf14
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2019-12-31 16:35:02 +0530

    Initial commit

--- suggest.php
+++ suggest.php
@@ -1,48 +1,62 @@
 <?php 
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$details = $_POST["details"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-echo "<pre>";
-$email_body = "";
-$email_body .= "Name: $name\n";
-$email_body .= "Email: $email\n";
-$email_body .= "Details: $details\n";
-echo $email_body;
-echo "</pre>";
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $details = $_POST["details"];
 
-// To do: Send email 
-header("location:thanks.php");
+  echo "<pre>";
+  $email_body = "";
+  $email_body .= "Name: $name\n";
+  $email_body .= "Email: $email\n";
+  $email_body .= "Details: $details\n";
+  echo $email_body;
+  echo "</pre>";
+
+  // To do: Send email 
+  header("location:suggest.php?status=thanks"); 
+
+}
 
 $pageTitle = "Suggest a Media Item";
 $section = 'suggest';
 
-include("inc/header.php"); 
+include("inc/header.php");
 
 ?>
 
   <div class="section page">
-    <h1><?php echo $pageTitle; ?></h1>
-    <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
-    <form method="post" action="process.php">
-      <table>
-        <tr>
-          <th><label for="name">Name:</label></th>
-          <td><input type="text" id="name" name="name"></td>
-        </tr>
-        <tr>
-          <th><label for="email">Email</label></th>
-          <td><input type="email" id="email" name="email"></td>
-        </tr>
-        <tr>
-          <th><label for="details">Suggest Item Details</label></th>
-          <td><textarea name="details" id="details"></textarea></td>
-        </tr>
-      </table>
+    <div class="wrapper">
+      <h1><?php echo $pageTitle; ?></h1>
 
-      <input type="submit" value="Send">
-    </form>
+    <?php 
+    if(isset($_GET["status"]) && isset($_GET["status"]) == "thanks") { 
+      echo "<p>Thank You! Thanks for your email, I&rsquo;ll check out your suggesstiomn shortly.</p>";
+    } else {
+    ?>
+      <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+      <form method="post" action="suggest.php">
+        <table>
+          <tr>
+            <th><label for="name">Name:</label></th>
+            <td><input type="text" id="name" name="name"></td>
+          </tr>
+          <tr>
+            <th><label for="email">Email</label></th>
+            <td><input type="email" id="email" name="email"></td>
+          </tr>
+          <tr>
+            <th><label for="details">Suggest Item Details</label></th>
+            <td><textarea name="details" id="details"></textarea></td>
+          </tr>
+        </table>
+
+        <input type="submit" value="Send">
+      </form>
+    <?php } ?>
+
+    </div>
   </div>
 
   <?php include("inc/footer.php"); ?>
@@ -50,4 +64,6 @@ include("inc/header.php");
 
 
 
-  
+
+
+
